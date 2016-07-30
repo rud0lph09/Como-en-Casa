5b81677cef87355f8e2b994cc10c7050c9f23c5c
Jonathan Velàzquez <jonhysiker@icloud.com>
2016-07-30 04:39:40 -0500

    Video Background Configuration

--- Como en Casa/LoginViewController.swift
+++ Como en Casa/LoginViewController.swift
@@ -7,12 +7,44 @@
 //
 
 import UIKit
+import AVKit
+import AVFoundation
 
 class LoginViewController: UIViewController {
 
+    var player: AVPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func configVideo(){
+        let videoURL: NSURL = NSBundle.mainBundle().URLForResource("video", withExtension: "mov")!
+        
+        player = AVPlayer(URL: videoURL)
+        player?.actionAtItemEnd = .None
+        player?.muted = true
+        
+        let playerLayer = AVPlayerLayer(player: player)
+        playerLayer.videoGravity = AVLayerVideoGravityResizeAspectFill
+        playerLayer.zPosition = -1
+        
+        playerLayer.frame = view.frame
+        
+        view.layer.addSublayer(playerLayer)
+        
+        player?.play()
+        
+        NSNotificationCenter.defaultCenter().addObserver(self,
+                                                         selector: #selector(LoginViewController.loopVideo),
+                                                         name: AVPlayerItemDidPlayToEndTimeNotification,
+                                                         object: nil)
+    }
+    
+    func loopVideo() {
+        player?.seekToTime(kCMTimeZero)
+        player?.play()
     }
 
     override func didReceiveMemoryWarning() {
