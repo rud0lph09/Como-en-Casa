4eb24262bfa0a4e1248b6c6738ce9ec703d35dfb
Rodolfo Castillo <raindeerdeveloper@gmail.com>
2016-07-30 02:38:49 -0500

    Initial Commit

---
+++ Como en Casa/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Como en Casa
+//
+//  Created by Rodolfo Castillo on 7/30/16.
+//  Copyright © 2016 Rodolfo Castillo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
