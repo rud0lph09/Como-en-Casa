bce24fbd6697dfa139c5e80d11f0b5f2d16d5fa5
Jonathan Velàzquez <jonhysiker@icloud.com>
2016-07-30 14:33:22 -0500

    Agregue openURL al AppDelegate

--- Como en Casa/AppDelegate.swift
+++ Como en Casa/AppDelegate.swift
@@ -23,6 +23,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         return true
     }
+    
+    func application(application: UIApplication, openURL url: NSURL, sourceApplication: String?, annotation: AnyObject) -> Bool {
+        
+        return FBSDKApplicationDelegate.sharedInstance().application(application, openURL: url, sourceApplication: sourceApplication, annotation: annotation)
+    }
 
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
--- Como en Casa/LoginViewController.swift
+++ Como en Casa/LoginViewController.swift
@@ -60,7 +60,10 @@ class LoginViewController: UIViewController, FBSDKLoginButtonDelegate {
     }
     
     func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!) {
-        
+        if (result.isCancelled == false && error == nil ){
+            let nv = self.storyboard?.instantiateViewControllerWithIdentifier("FoodMenuController")
+            self.presentViewController(nv!, animated: true, completion: nil)
+        }
     }
     
 
