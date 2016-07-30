671ee8b47b0603b97cb17cd4ed67166463f1ae7f
Rodolfo Castillo <raindeerdeveloper@gmail.com>
2016-07-30 03:00:30 -0500

    Added Files for Controllers
    Johnny: Login & TicketViewController
    Rodolfo: FoodMenu & MapsController

---
+++ Como en Casa/MapsController.swift
@@ -0,0 +1,13 @@
+//
+//  MapsController.swift
+//  Como en Casa
+//
+//  Created by Rodolfo Castillo on 7/30/16.
+//  Copyright © 2016 Rodolfo Castillo. All rights reserved.
+//
+
+import UIKit
+
+class MapsController: UIViewController {
+    
+}
---
+++ Como en Casa/TicketViewController.swift
@@ -0,0 +1,13 @@
+//
+//  TicketViewController.swift
+//  Como en Casa
+//
+//  Created by Rodolfo Castillo on 7/30/16.
+//  Copyright © 2016 Rodolfo Castillo. All rights reserved.
+//
+
+import UIKit
+
+class TicketViewController: UIViewController {
+    
+}
