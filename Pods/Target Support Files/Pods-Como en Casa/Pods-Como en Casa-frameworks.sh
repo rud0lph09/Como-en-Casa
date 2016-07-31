782b4dbe9a46e34fa4e7cdcb87d98e84bf808f23
Jonathan Velazquez <jonhysiker@icloud.com>
2016-07-31 04:37:56 -0500

    Agregando Pods de Alamofire, SwiftyJson y CardIO

--- Pods/Target Support Files/Pods-Como en Casa/Pods-Como en Casa-frameworks.sh
+++ Pods/Target Support Files/Pods-Como en Casa/Pods-Como en Casa-frameworks.sh
@@ -84,8 +84,12 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "Pods-Como en Casa/Alamofire.framework"
   install_framework "Pods-Como en Casa/NAExpandableTableController.framework"
+  install_framework "Pods-Como en Casa/SwiftyJSON.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "Pods-Como en Casa/Alamofire.framework"
   install_framework "Pods-Como en Casa/NAExpandableTableController.framework"
+  install_framework "Pods-Como en Casa/SwiftyJSON.framework"
 fi
